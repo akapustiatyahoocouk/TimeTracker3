9021127ec560d797bec1b5eb4a76521189265000
Andrey Kapustin <akapusti@yahoo.co.uk>
2025-10-02 23:47:21 +0300

    .

--- Dist.QT/Debug/make.bat
+++ Dist.QT/Debug/make.bat
@@ -2,6 +2,7 @@ call clean.bat
 
 copy ..\..\Bin.QT\Debug\*.dll .
 copy ..\..\Bin.QT\Debug\*.exe .
+copy ..\..\Bin.QT\Debug\*.pdb .
 
 c:\cygwin64\bin\find . -name "*.dll" | xargs C:\Qt\6.8.0\msvc2022_64\bin\windeployqt
 c:\cygwin64\bin\find . -name "*.exe" | xargs C:\Qt\6.8.0\msvc2022_64\bin\windeployqt
