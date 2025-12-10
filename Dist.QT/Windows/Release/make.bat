a54f9c9bee6d4ac09e5ceb3d660a7b006633b171
Andrey Kapustin <akapusti@yahoo.co.uk>
2025-12-10 04:36:43 +0300

    .

--- Dist.QT/Windows/Release/make.bat
+++ Dist.QT/Windows/Release/make.bat
@@ -4,5 +4,7 @@ copy ..\..\..\Bin.QT\Release\*.dll .
 copy ..\..\..\Bin.QT\Release\*.exe .
 cp -r ..\..\..\Bin.QT\Release\Help .
 
-c:\cygwin64\bin\find . -name "*.dll" | xargs C:\Qt\6.10.1\msvc2022_64\bin\windeployqt
-c:\cygwin64\bin\find . -name "*.exe" | xargs C:\Qt\6.10.1\msvc2022_64\bin\windeployqt
+set QT_BIN_DIR=C:\Qt\6.10.1\msvc2022_64\bin
+
+c:\cygwin64\bin\find . -name "*.dll" | xargs %QT_BIN_DIR%\windeployqt
+c:\cygwin64\bin\find . -name "*.exe" | xargs %QT_BIN_DIR%\windeployqt
