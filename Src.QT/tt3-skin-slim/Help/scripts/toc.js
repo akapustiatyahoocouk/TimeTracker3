073a1f6f10ab8953bf7b04c73a4eed9cdc4baf1f
Andrey Kapustin <akapusti@yahoo.co.uk>
2026-01-16 02:13:01 +0300

    .

---
+++ Src.QT/tt3-skin-slim/Help/scripts/toc.js
@@ -0,0 +1,26 @@
+﻿class Topic {
+  constructor(text, level, href) {
+    this.text = text;
+    this.level = level;
+    this.href = href;
+    this.children = [];
+    this.expanded = false;
+  }
+}
+
+const rootTopic = new Topic("", 0, "index.html");
+const topics = [rootTopic];
+console.log(rootTopic);
+
+const tocRe = new RegExp("^toc([1-6])$");
+for (const a of document.getElementsByTagName("a")) {
+  const matches = tocRe.exec(a.className);
+  //console.log(matches[1] + ": " + a.innerText + " -> " + a.href);
+  a.onclick = function() { 
+      window.parent.postMessage({
+        "type": "topicNav",
+        "topic": a.href
+    }, "*");
+    return false;
+  };
+}
