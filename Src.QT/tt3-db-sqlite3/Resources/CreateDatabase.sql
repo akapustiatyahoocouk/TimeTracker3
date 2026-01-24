808aa0033cf5ac5b153c1b041fd78add3f7cf09f
Andrey Kapustin <akapusti@yahoo.co.uk>
2026-01-25 00:59:24 +0300

    .

--- Src.QT/tt3-db-sqlite3/Resources/CreateDatabase.sql
+++ Src.QT/tt3-db-sqlite3/Resources/CreateDatabase.sql
@@ -20,3 +20,43 @@ CREATE TABLE [users] (
     CHECK(([enabled] = 'Y') OR ([enabled] = 'N'))
 );
 
+CREATE TABLE [accounts] (
+    [pk]                        INTEGER NOT NULL UNIQUE,
+    [fk_user]                   INTEGER NOT NULL,
+    [enabled]                   CHAR(1) NOT NULL,       --  'Y' or 'N'
+    [emailaddresses]            TEXT,                   --  '\n' - separated, NULL == none
+    [login]                     VARCHAR(127) NOT NULL,  --  as per DefaultValidator
+    [passwordhash]              CHAR(40) NOT NULL,      --  SHA-1 uppercase hexstring
+    [administrator]             CHAR(1) NOT NULL,       --  'Y' or 'N'
+    [manageusers]               CHAR(1) NOT NULL,       --  'Y' or 'N'
+    [manageactivitytypes]       CHAR(1) NOT NULL,       --  'Y' or 'N'
+    [managebeneficiaries]       CHAR(1) NOT NULL,       --  'Y' or 'N'
+    [manageworkloads]           CHAR(1) NOT NULL,       --  'Y' or 'N'
+    [managepublicactivities]    CHAR(1) NOT NULL,       --  'Y' or 'N'
+    [managepublictasks]         CHAR(1) NOT NULL,       --  'Y' or 'N'
+    [manageprivateactivities]   CHAR(1) NOT NULL,       --  'Y' or 'N'
+    [manageprivatetasks]        CHAR(1) NOT NULL,       --  'Y' or 'N'
+    [logwork]                   CHAR(1) NOT NULL,       --  'Y' or 'N'
+    [logevents]                 CHAR(1) NOT NULL,       --  'Y' or 'N'
+    [generatereports]           CHAR(1) NOT NULL,       --  'Y' or 'N'
+    [backupandrestore]          CHAR(1) NOT NULL,       --  'Y' or 'N'
+    PRIMARY KEY([pk]),
+    FOREIGN KEY([pk]) REFERENCES [objects]([pk]),
+    FOREIGN KEY([fk_user]) REFERENCES [users]([pk]),
+    CHECK(([enabled] = 'Y') OR ([enabled] = 'N')),
+    CHECK(([administrator] = 'Y') OR ([administrator] = 'N')),
+    CHECK(([manageusers] = 'Y') OR ([manageusers] = 'N')),
+    CHECK(([manageactivitytypes] = 'Y') OR ([manageactivitytypes] = 'N')),
+    CHECK(([managebeneficiaries] = 'Y') OR ([managebeneficiaries] = 'N')),
+    CHECK(([manageworkloads] = 'Y') OR ([manageworkloads] = 'N')),
+    CHECK(([managepublicactivities] = 'Y') OR ([managepublicactivities] = 'N')),
+    CHECK(([managepublictasks] = 'Y') OR ([managepublictasks] = 'N')),
+    CHECK(([manageprivateactivities] = 'Y') OR ([manageprivateactivities] = 'N')),
+    CHECK(([manageprivatetasks] = 'Y') OR ([manageprivatetasks] = 'N')),
+    CHECK(([logwork] = 'Y') OR ([logwork] = 'N')),
+    CHECK(([logevents] = 'Y') OR ([logevents] = 'N')),
+    CHECK(([generatereports] = 'Y') OR ([generatereports] = 'N')),
+    CHECK(([backupandrestore] = 'Y') OR ([backupandrestore] = 'N'))
+);
+CREATE INDEX [idx_account_user] ON [accounts] ([fk_user]);
+CREATE INDEX [idx_account_login] ON [accounts] ([login]);
